0e8368aaede75f06ecf59cc4fefa43b803218c17
DreamFly111 <1320027594@qq.com>
2016-06-20 11:42:12 +0800

    摄像头改版为竖条

--- clientApp/src/main/java/com/meplus/client/activity/CallActivity.java
+++ clientApp/src/main/java/com/meplus/client/activity/CallActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.meplus.activity.VideoActivity;
 import com.meplus.avos.objects.AVOSUser;
@@ -12,6 +13,7 @@ import com.meplus.events.EventUtils;
 import com.meplus.punub.Command;
 import com.meplus.punub.CommandEvent;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnTouch;
 
@@ -20,6 +22,32 @@ import butterknife.OnTouch;
  */
 public class CallActivity extends VideoActivity {
     private static final String TAG = CallActivity.class.getSimpleName();
+
+    boolean isButton = true;
+
+    @Bind(R.id.zero)
+    ImageButton zero;
+
+    @Bind(R.id.one)
+    ImageButton one;
+
+    @Bind(R.id.two)
+    ImageButton two;
+
+    @Bind(R.id.three)
+    ImageButton three;
+
+    @Bind(R.id.four)
+    ImageButton four;
+
+    @Bind(R.id.five)
+    ImageButton five;
+
+    @Bind(R.id.six)
+    ImageButton six;
+
+    @Bind(R.id.seven)
+    ImageButton seven;
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -65,27 +93,106 @@ public class CallActivity extends VideoActivity {
                 message = Command.ACTION_DOWN;
                 break;
             case R.id.zero:
+                if(isButton) {
+                    zero.setEnabled(false);
+                    one.setEnabled(true);
+                    two.setEnabled(true);
+                    three.setEnabled(true);
+                    four.setEnabled(true);
+                    five.setEnabled(true);
+                    six.setEnabled(true);
+                    seven.setEnabled(true);
+                }
                 message = Command.ZERO;
                 break;
             case R.id.one:
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(false);
+                    two.setEnabled(true);
+                    three.setEnabled(true);
+                    four.setEnabled(true);
+                    five.setEnabled(true);
+                    six.setEnabled(true);
+                    seven.setEnabled(true);
+                }
                 message = Command.ONE;
                 break;
             case R.id.two:
-                message = Command.TWO;
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(true);
+                    two.setEnabled(false);
+                    three.setEnabled(true);
+                    four.setEnabled(true);
+                    five.setEnabled(true);
+                    six.setEnabled(true);
+                    seven.setEnabled(true);
+                }                message = Command.TWO;
                 break;
             case R.id.three:
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(true);
+                    two.setEnabled(true);
+                    three.setEnabled(false);
+                    four.setEnabled(true);
+                    five.setEnabled(true);
+                    six.setEnabled(true);
+                    seven.setEnabled(true);
+                }
                 message = Command.THREE;
                 break;
             case R.id.four:
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(true);
+                    two.setEnabled(true);
+                    three.setEnabled(true);
+                    four.setEnabled(false);
+                    five.setEnabled(true);
+                    six.setEnabled(true);
+                    seven.setEnabled(true);
+                }
                 message = Command.FOUR;
                 break;
             case R.id.five:
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(true);
+                    two.setEnabled(true);
+                    three.setEnabled(true);
+                    four.setEnabled(true);
+                    five.setEnabled(false);
+                    six.setEnabled(true);
+                    seven.setEnabled(true);
+                }
                 message = Command.FIVE;
                 break;
             case R.id.six:
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(true);
+                    two.setEnabled(true);
+                    three.setEnabled(true);
+                    four.setEnabled(true);
+                    five.setEnabled(true);
+                    six.setEnabled(false);
+                    seven.setEnabled(true);
+                }
                 message = Command.SIX;
                 break;
             case R.id.seven:
+                if(isButton) {
+                    zero.setEnabled(true);
+                    one.setEnabled(true);
+                    two.setEnabled(true);
+                    three.setEnabled(true);
+                    four.setEnabled(true);
+                    five.setEnabled(true);
+                    six.setEnabled(true);
+                    seven.setEnabled(false);
+                }
                 message = Command.SEVEN;
                 break;
         }
