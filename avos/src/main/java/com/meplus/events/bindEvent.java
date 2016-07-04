fdcae2469c88fafb4d89b5691d7a98c559531060
DreamFly111 <1320027594@qq.com>
2016-07-04 11:40:53 +0800

    bindEvent--绑定提示

---
+++ avos/src/main/java/com/meplus/events/bindEvent.java
@@ -0,0 +1,16 @@
+package com.meplus.events;
+
+import cn.trinea.android.common.util.ToastUtils;
+
+/**
+ * Created by dandanba on 3/11/16.
+ * 登出
+ */
+public class bindEvent{
+
+    public bindEvent() {}
+
+    public String getMessage(){
+        return "该机器人已经被绑定！";
+    }
+}
