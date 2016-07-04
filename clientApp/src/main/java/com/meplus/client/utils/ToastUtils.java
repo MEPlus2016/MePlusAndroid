ae27a677ee880f0dc6792515a88d1d6ca25c5336
DreamFly111 <1320027594@qq.com>
2016-07-04 12:11:30 +0800

    toastUtils

---
+++ clientApp/src/main/java/com/meplus/client/utils/ToastUtils.java
@@ -0,0 +1,24 @@
+package com.meplus.client.utils;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+/**
+ * Created by Dream on 2016/7/3.
+ */
+public class ToastUtils {
+    public static void toShowToast(final Activity act,final String msg) {
+        //判断是否是主线程
+        if("main".equals(Thread.currentThread().getName())){
+            Toast.makeText(act, msg, Toast.LENGTH_LONG).show();
+        }else{
+            act.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(act, msg, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
